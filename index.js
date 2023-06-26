7c2aa0c5f315b6611dce44cac4a4296797b805bb
Rishi <rishijoshi483@gmail.com>
2023-06-26 22:04:08 +0530

    fronted address changed

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(morgan('common'));
 app.use(cookieParser());
 app.use(cors({
     credentials:true,
-    origin:"http://localhost:3000"
+    origin:process.env.FRONTEND_ADDRESS
 }));
 
 app.use('/auth',authRouter);
