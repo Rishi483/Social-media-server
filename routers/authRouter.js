cfa17a18f7da26ad2aa44aec8796a0f39d16760d
Rishi Joshi <97284468+Rishi483@users.noreply.github.com>
2023-06-27 03:40:25 +0530

    Update authRouter.js

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -3,6 +3,6 @@ const authController=require('../controllers/authController');
 
 router.post('/login',authController.loginController);
 router.post('/signup',authController.signupController);
-router.get('/refresh',authController.refreshAccessTokenController);
+router.post('/refresh',authController.refreshAccessTokenController);
 router.post('/logout',authController.logOutController);
 module.exports=router;
