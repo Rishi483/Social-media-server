bf4b2c1c74a66a6337553b750aa5756ea9ce7994
Rishi Joshi <97284468+Rishi483@users.noreply.github.com>
2023-06-27 15:23:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,6 +20,13 @@ cloudinary.config({
   });
 
 //middlewares
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Origin', true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+  next();
+});
 app.use(express.json({limit:'10mb'}));
 app.use(morgan('common'));
 app.use(cookieParser());
